ed9596786b3693c43926cced5633311008d49a2f
AlfreGonzales <alfregonzales2003@gmail.com>
2025-11-30 14:21:52 -0400

    first commit

---
+++ src/test/java/project/hotel_reservations/HotelReservationsApplicationTests.java
@@ -0,0 +1,13 @@
+package project.hotel_reservations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelReservationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
