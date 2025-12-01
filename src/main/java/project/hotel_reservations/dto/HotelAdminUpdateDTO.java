0a4ea4a3b593e666afb3392714d68258c1b0be44
AlfreGonzales <alfregonzales2003@gmail.com>
2025-12-01 03:15:24 -0400

    feat: add hotel admin CRUD

---
+++ src/main/java/project/hotel_reservations/dto/HotelAdminUpdateDTO.java
@@ -0,0 +1,23 @@
+package project.hotel_reservations.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+
+public record HotelAdminUpdateDTO(
+        @Size(min = 1, message = "Identification must not be empty")
+        String identification,
+
+        @Size(min = 1, message = "Name must not be empty")
+        String name,
+
+        @Size(min = 1, message = "Email must not be empty")
+        @Email(message = "Email must be valid")
+        String email,
+
+        @Size(min = 1, message = "Position must not be empty")
+        String position,
+
+        @Size(min = 1, message = "Shift must not be empty")
+        String shift
+) {
+}
