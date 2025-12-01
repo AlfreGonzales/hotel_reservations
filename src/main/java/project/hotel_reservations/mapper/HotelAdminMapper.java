fe431d3d8f757fea59b430b34829221d3873ab51
AlfreGonzales <alfregonzales2003@gmail.com>
2025-12-01 03:17:08 -0400

    feat: add mapstruct, GlobalExceptionHandler and swagger

--- src/main/java/project/hotel_reservations/mapper/HotelAdminMapper.java
+++ src/main/java/project/hotel_reservations/mapper/HotelAdminMapper.java
@@ -1,7 +1,6 @@
 package project.hotel_reservations.mapper;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import project.hotel_reservations.dto.HotelAdminResponseDTO;
@@ -11,7 +10,6 @@ import project.hotel_reservations.model.HotelAdmin;
 @Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface HotelAdminMapper {
 
-    @Mapping(source = "hotel.id", target = "hotelId")
     HotelAdminResponseDTO toDto(HotelAdmin entity);
 
     void toEntity(HotelAdminUpdateDTO dto, @MappingTarget HotelAdmin entity);
