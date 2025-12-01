2c4cf54705dbc5d77040786d45e692cd3ba73206
AlfreGonzales <alfregonzales2003@gmail.com>
2025-12-01 03:17:48 -0400

    feat: add guest CRUD

--- src/main/java/project/hotel_reservations/dto/GuestResponseDTO.java
+++ src/main/java/project/hotel_reservations/dto/GuestResponseDTO.java
@@ -1,12 +1,9 @@
 package project.hotel_reservations.dto;
 
-import lombok.Builder;
-
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-@Builder
 public record GuestResponseDTO(
         UUID id,
         String identification,
