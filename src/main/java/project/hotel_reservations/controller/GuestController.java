2238a61dc4d6ced883730d4b1c641496c86fd7fe
CrhistianT7 <andrewturpo573@gmail.com>
2025-12-02 19:41:32 -0500

    refactor: change endpoint authorization to use PreAuthorize

--- src/main/java/project/hotel_reservations/controller/GuestController.java
+++ src/main/java/project/hotel_reservations/controller/GuestController.java
@@ -10,6 +10,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import project.hotel_reservations.dto.GuestCreateDTO;
 import project.hotel_reservations.dto.GuestResponseDTO;
@@ -46,6 +47,7 @@ public class GuestController {
             @ApiResponse(responseCode = "200", description = "List of guests returned successfully")
     })
     @GetMapping
+    @PreAuthorize("hasRole('guest')")
     public ResponseEntity<List<GuestResponseDTO>> findAll() {
         return ResponseEntity.ok(service.findAll());
     }
