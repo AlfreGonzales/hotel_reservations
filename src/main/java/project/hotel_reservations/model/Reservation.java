28a355778d3181eedb1da75af8ac891a83f18006
AlfreGonzales <alfregonzales2003@gmail.com>
2025-12-03 03:16:43 -0400

    feat: add reservation entity, DTOs and custom validation

---
+++ src/main/java/project/hotel_reservations/model/Reservation.java
@@ -0,0 +1,43 @@
+package project.hotel_reservations.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Table(name = "reservations")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Reservation {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(name = "check_in_date", nullable = false)
+    private LocalDate checkInDate;
+
+    @Column(name = "check_out_date", nullable = false)
+    private LocalDate checkOutDate;
+
+    @Column(name = "people_count", nullable = false)
+    private Integer peopleCount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    @Builder.Default
+    private ReservationStatus status = ReservationStatus.PENDING;
+
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "room_id", nullable = false)
+    private Room room;
+
+    @ManyToOne(fetch = FetchType.EAGER, optional = false)
+    @JoinColumn(name = "guest_id", nullable = false)
+    private Guest guest;
+}
