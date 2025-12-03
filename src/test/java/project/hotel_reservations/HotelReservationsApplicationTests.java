6442c1eb27d8a2c72d5e7da09780474003e6e83f
CrhistianT7 <andrewturpo573@gmail.com>
2025-12-02 20:11:35 -0500

    fix: add context

--- src/test/java/project/hotel_reservations/HotelReservationsApplicationTests.java
+++ src/test/java/project/hotel_reservations/HotelReservationsApplicationTests.java
@@ -1,11 +1,9 @@
 package project.hotel_reservations;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-@Disabled("Disabled in CI because full context loading requires environment vars")
+@SpringBootTest(classes = HotelReservationsApplication.class)
 class HotelReservationsApplicationTests {
 
 	@Test
