1f7ed18c833c291e3725bc82ad0bf528ae634d7f
CrhistianT7 <andrewturpo573@gmail.com>
2025-12-02 20:11:35 -0500

    fix: add context

--- src/test/java/project/hotel_reservations/HotelReservationsApplicationTests.java
+++ src/test/java/project/hotel_reservations/HotelReservationsApplicationTests.java
@@ -3,7 +3,7 @@ package project.hotel_reservations;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = HotelReservationsApplication.class)
 class HotelReservationsApplicationTests {
 
 	@Test
