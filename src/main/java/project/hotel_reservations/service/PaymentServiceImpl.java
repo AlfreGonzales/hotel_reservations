cdfa0f504606526ca024b0c43bbc0895a5f9eb72
AlfreGonzales <alfregonzales2003@gmail.com>
2025-12-04 01:19:13 -0400

    feat: add factory method pattern when processing a payment

---
+++ src/main/java/project/hotel_reservations/service/PaymentServiceImpl.java
@@ -0,0 +1,69 @@
+package project.hotel_reservations.service;
+
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import project.hotel_reservations.dto.PaymentCreateDTO;
+import project.hotel_reservations.model.Payment;
+import project.hotel_reservations.model.PaymentMethod;
+import project.hotel_reservations.model.PaymentPlatform;
+import project.hotel_reservations.payment.factory.PaymentProcessorFactory;
+import project.hotel_reservations.payment.processor.PaymentProcessor;
+import project.hotel_reservations.repository.PaymentPlatformRepository;
+import project.hotel_reservations.repository.PaymentRepository;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentServiceImpl implements PaymentService {
+
+    private final PaymentRepository repository;
+    private final PaymentPlatformRepository paymentPlatformRepository;
+
+    /**
+     * Processes and creates a payment
+     *
+     * @param req DTO with creation data
+     * @return DTO of the created payment
+     * @throws IllegalArgumentException   if payment platform is invalid
+     * @throws EntityNotFoundException    if payment platform not found
+     */
+    @Override
+    @Transactional
+    public Payment processPayment(PaymentCreateDTO req) {
+        Payment entity = Payment.builder()
+                .totalAmount(req.totalAmount())
+                .paymentMethod(req.paymentMethod())
+                .reservation(req.reservation())
+                .build();
+
+        if (entity.getPaymentMethod() == PaymentMethod.CASH) {
+
+            if (req.paymentPlatformId() != null) {
+                throw new IllegalArgumentException("CASH payments must not specify a payment platform");
+            }
+
+            return repository.save(entity);
+        }
+
+        if (entity.getPaymentMethod() == PaymentMethod.TRANSFER) {
+
+            if (req.paymentPlatformId() == null) {
+                throw new IllegalArgumentException("TRANSFER payments require a payment platform");
+            }
+
+            PaymentPlatform paymentPlatform = paymentPlatformRepository.findById(req.paymentPlatformId())
+                    .orElseThrow(() -> new EntityNotFoundException("Payment platform not found"));
+
+            entity.setPaymentPlatform(paymentPlatform);
+
+            PaymentProcessor processor = PaymentProcessorFactory.getProcessor(paymentPlatform);
+
+            processor.process(entity);
+
+            return repository.save(entity);
+        }
+
+        return null;
+    }
+}
