6971fa42caf7fcc5e4d8efb1e577a4d7034eb225
AlfreGonzales <alfregonzales2003@gmail.com>
2025-12-04 23:26:22 -0400

    feat: add logs

--- src/main/java/project/hotel_reservations/factory/PayPalProcessor.java
+++ src/main/java/project/hotel_reservations/factory/PayPalProcessor.java
@@ -1,11 +1,14 @@
 package project.hotel_reservations.factory;
 
+import lombok.extern.slf4j.Slf4j;
 import project.hotel_reservations.model.Payment;
 
+@Slf4j
 public class PayPalProcessor implements PaymentProcessor {
 
     @Override
     public void process(Payment payment) {
-        System.out.println("Processing payment with PayPal...");
+        log.info("PayPal processing started for payment");
+        log.info("PayPal processing completed for payment");
     }
 }
